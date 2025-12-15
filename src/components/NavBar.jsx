b6ca004b2b78a0ab867b9e41c1544793bb78a61f
Swastik <sharmaspeedx29@gmail.com>
2025-12-15 19:46:16 +0530

    about section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
       { backgroundColor: "transparent" },
       {
         backgroundColor: "#00000050",
-        backgroundFilter: "blur(10px)",
+        backdropFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
       }
@@ -27,7 +27,7 @@ const NavBar = () => {
   return (
     <nav>
       <div>
-        <a href="#home" className="flex items-center gap-2">
+        <a href="#hero" className="flex items-center gap-2">
           <img src="/images/logo.png" alt="logo" />
           <p>Velvet Pour</p>
         </a>
